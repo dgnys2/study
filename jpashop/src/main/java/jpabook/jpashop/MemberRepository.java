451e3a615d5b10e7d372abb508b0b0ef29be9806
Daniel Kang <dgnys2@gmail.com>
2020-04-15 12:08:39 +0900

    added jpa project to study

---
+++ jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    // 이 annotation 이 있으면 entitymanager 를 spring boot가 주입해준다.
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class,id);
+    }
+
+}
