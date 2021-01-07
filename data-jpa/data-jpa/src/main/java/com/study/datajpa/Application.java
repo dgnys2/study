b364b10624e4512a6bd53c39c6871eb543cdf4c2
Daniel Kang <dgnys2@gmail.com>
2021-01-07 21:27:38 +0900

    study example of jpa

--- data-jpa/data-jpa/src/main/java/com/study/datajpa/Application.java
+++ data-jpa/data-jpa/src/main/java/com/study/datajpa/Application.java
@@ -2,8 +2,10 @@ package com.study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@EnableJpaRepositories(basePackages = "com.sutdy.datajpa.repository")
 public class Application {
 
 	public static void main(String[] args) {
--- data-jpa/data-jpa/src/test/java/com/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/data-jpa/src/test/java/com/study/datajpa/repository/MemberRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.study.datajpa.repository;
 
+import com.study.datajpa.dto.MemberDto;
 import com.study.datajpa.entity.Member;
+import com.study.datajpa.entity.Team;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,7 +21,9 @@ import static org.junit.jupiter.api.Assertions.*;
 @Transactional
 @Rollback(false)
 class MemberRepositoryTest {
+
     @Autowired MemberRepository memberRepository;
+    @Autowired TeamRepository teamRepository;
 
     @Test
     public void testMember(){
@@ -31,4 +37,152 @@ class MemberRepositoryTest {
         assertThat(findMember).isEqualTo(member);
 
     }
+
+    @Test
+    public void basicCRUD(){
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+
+        // verify list get
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        // verify count
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        //verify remove
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long deletedCount = memberRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
+
+    }
+
+    @Test
+    public void findByUsernameAndAgeGreaterThan(){
+
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+
+
+        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.size()).isEqualTo(1);
+
+    }
+
+    @Test
+    public void testNamedQuery(){
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsername("AAA");
+
+        Member findMember = result.get(0);
+
+        assertThat(findMember).isEqualTo(m1);
+
+    }
+
+
+    @Test
+    public void testQuery(){
+
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> findMember = memberRepository.findUser("AAA",10);
+        assertThat(findMember.get(0)).isEqualTo(m1);
+
+    }
+
+    @Test
+    public void findUsernameList(){
+
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<String> usernameList = memberRepository.findUsernameList();
+
+        usernameList.stream().forEach(u-> System.out.println("s="+ u));
+
+    }
+
+
+    @Test
+    public void findMemberDto(){
+
+        Team team = new Team("teamA");
+        teamRepository.save(team);
+
+        Member m1 = new Member("AAA", 10);
+        m1.setTeam(team);
+        memberRepository.save(m1);
+
+        List<MemberDto> memberDto = memberRepository.findMemberDto();
+
+        memberDto.stream().forEach(u-> System.out.println("dto="+ u));
+
+    }
+
+
+    @Test
+    public void findByNames(){
+
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> usernameList = memberRepository.findByNames(Arrays.asList("AAA","BBB"));
+
+        usernameList.stream().forEach(u-> System.out.println("s="+ u));
+
+    }
+
+    @Test
+    public void returnType(){
+
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> userList = memberRepository.findListByUsername("AAA"); // 이게 없다면 NULL 이 아니라 empty collection 을 제공
+
+        Member findMember = memberRepository.findMemberByUsername("AAA"); // 단건이면 결과가 없으면 null 이다.
+
+        Optional<Member> findOptionalMember = memberRepository.findOptionalByUsername("AAA"); // 없으면 Optional Empty 가 찍힌다.
+
+
+    }
+
 }
